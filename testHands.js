591b21a745495608d781681ea820f1af5e39b970
1Regina <1reginacheong@gmail.com>
2021-12-21 00:35:08 +0800

    hands for testing

---
+++ testHands.js
@@ -0,0 +1,350 @@
+// straight
+const straits = [
+  {
+    suitSymbol: '♣',
+    suit: 'clubs',
+    name: '3',
+    displayName: '3',
+    colour: 'black',
+    rank: 3,
+  },
+  {
+    suitSymbol: '♣',
+    suit: 'clubs',
+    name: '4',
+    displayName: '4',
+    colour: 'black',
+    rank: 4,
+  },
+  {
+    suitSymbol: '♣',
+    suit: 'clubs',
+    name: '5',
+    displayName: '5',
+    colour: 'black',
+    rank: 5,
+  },
+  {
+    suitSymbol: '♣',
+    suit: 'clubs',
+    name: '6',
+    displayName: '6',
+    colour: 'black',
+    rank: 6,
+  },
+  {
+    suitSymbol: '♣',
+    suit: 'clubs',
+    name: '7',
+    displayName: '7',
+    colour: 'black',
+    rank: 7,
+  },
+];
+
+const oddStraits = [
+  {
+    suitSymbol: '♦',
+    suit: 'diamonds',
+    name: '3',
+    displayName: '3',
+    colour: 'red',
+    rank: 3,
+  },
+  {
+    suitSymbol: '♦',
+    suit: 'diamonds',
+    name: '4',
+    displayName: '4',
+    colour: 'red',
+    rank: 4,
+  },
+  {
+    suitSymbol: '♣',
+    suit: 'clubs',
+    name: '5',
+    displayName: '5',
+    colour: 'black',
+    rank: 5,
+  },
+  {
+    suitSymbol: '♣',
+    suit: 'clubs',
+    name: '6',
+    displayName: '6',
+    colour: 'black',
+    rank: 6,
+  },
+  {
+    suitSymbol: '♣',
+    suit: 'clubs',
+    name: '7',
+    displayName: '7',
+    colour: 'black',
+    rank: 7,
+  },
+];
+
+// royal flush
+const royalty = [
+  {
+    suitSymbol: '♦',
+    suit: 'diamonds',
+    name: 'ace',
+    displayName: 'A',
+    colour: 'red',
+    rank: 1,
+  },
+  {
+    suitSymbol: '♦',
+    suit: 'diamonds',
+    name: '10',
+    displayName: '10',
+    colour: 'red',
+    rank: 10,
+  },
+  {
+    suitSymbol: '♦',
+    suit: 'diamonds',
+    name: 'jack',
+    displayName: 'J',
+    colour: 'red',
+    rank: 11,
+  },
+  {
+    suitSymbol: '♦',
+    suit: 'diamonds',
+    name: 'queen',
+    displayName: 'Q',
+    colour: 'red',
+    rank: 12,
+  },
+  {
+    suitSymbol: '♦',
+    suit: 'diamonds',
+    name: 'king',
+    displayName: 'K',
+    colour: 'red',
+    rank: 13,
+  },
+];
+
+// one pair
+const twin = [
+  {
+    suitSymbol: '♥',
+    suit: 'hearts',
+    name: '6',
+    displayName: '6',
+    colour: 'red',
+    rank: 6,
+  },
+  {
+    suitSymbol: '♥',
+    suit: 'hearts',
+    name: '7',
+    displayName: '7',
+    colour: 'red',
+    rank: 7,
+  },
+  {
+    suitSymbol: '♥',
+    suit: 'hearts',
+    name: '8',
+    displayName: '8',
+    colour: 'red',
+    rank: 8,
+  },
+  {
+    suitSymbol: '♥',
+    suit: 'hearts',
+    name: '9',
+    displayName: '9',
+    colour: 'red',
+    rank: 9,
+  },
+  {
+    suitSymbol: '♠',
+    suit: 'spades',
+    name: '9',
+    displayName: '9',
+    colour: 'black',
+    rank: 9,
+  },
+];
+
+// two pairs
+const twinTwin = [
+  {
+    suitSymbol: '♥',
+    suit: 'hearts',
+    name: '7',
+    displayName: '7',
+    colour: 'red',
+    rank: 7,
+  },
+  {
+    suitSymbol: '♥',
+    suit: 'hearts',
+    name: '8',
+    displayName: '8',
+    colour: 'red',
+    rank: 8,
+  },
+  {
+    suitSymbol: '♥',
+    suit: 'hearts',
+    name: '9',
+    displayName: '9',
+    colour: 'red',
+    rank: 9,
+  },
+  {
+    suitSymbol: '♦',
+    suit: 'diamonds',
+    name: '7',
+    displayName: '7',
+    colour: 'red',
+    rank: 7,
+  },
+  {
+    suitSymbol: '♠',
+    suit: 'spades',
+    name: '9',
+    displayName: '9',
+    colour: 'black',
+    rank: 9,
+  },
+];
+
+// three of a kind
+const triple = [
+  {
+    suitSymbol: '♥',
+    suit: 'hearts',
+    name: '7',
+    displayName: '7',
+    colour: 'red',
+    rank: 7,
+  },
+  {
+    suitSymbol: '♥',
+    suit: 'hearts',
+    name: '8',
+    displayName: '8',
+    colour: 'red',
+    rank: 8,
+  },
+  {
+    suitSymbol: '♥',
+    suit: 'hearts',
+    name: '9',
+    displayName: '9',
+    colour: 'red',
+    rank: 9,
+  },
+  {
+    suitSymbol: '♦',
+    suit: 'diamonds',
+    name: '9',
+    displayName: '9',
+    colour: 'red',
+    rank: 9,
+  },
+  {
+    suitSymbol: '♠',
+    suit: 'spades',
+    name: '9',
+    displayName: '9',
+    colour: 'black',
+    rank: 9,
+  },
+];
+
+// fullhouse
+const fully = [
+  {
+    suitSymbol: '♥',
+    suit: 'hearts',
+    name: '8',
+    displayName: '8',
+    colour: 'red',
+    rank: 8,
+  },
+  {
+    suitSymbol: '♥',
+    suit: 'hearts',
+    name: '9',
+    displayName: '9',
+    colour: 'red',
+    rank: 9,
+  },
+  {
+    suitSymbol: '♦',
+    suit: 'diamonds',
+    name: '8',
+    displayName: '8',
+    colour: 'red',
+    rank: 8,
+  },
+  {
+    suitSymbol: '♠',
+    suit: 'spades',
+    name: '9',
+    displayName: '9',
+    colour: 'black',
+    rank: 9,
+  },
+  {
+    suitSymbol: '♣',
+    suit: 'clubs',
+    name: '8',
+    displayName: '8',
+    colour: 'black',
+    rank: 8,
+  },
+];
+
+// four of a kind
+const quadsi = [
+  {
+    suitSymbol: '♥',
+    suit: 'hearts',
+    name: '8',
+    displayName: '8',
+    colour: 'red',
+    rank: 8,
+  },
+  {
+    suitSymbol: '♥',
+    suit: 'hearts',
+    name: '9',
+    displayName: '9',
+    colour: 'red',
+    rank: 9,
+  },
+  {
+    suitSymbol: '♦',
+    suit: 'diamonds',
+    name: '9',
+    displayName: '9',
+    colour: 'red',
+    rank: 9,
+  },
+  {
+    suitSymbol: '♠',
+    suit: 'spades',
+    name: '9',
+    displayName: '9',
+    colour: 'black',
+    rank: 9,
+  },
+  {
+    suitSymbol: '♣',
+    suit: 'clubs',
+    name: '9',
+    displayName: '9',
+    colour: 'black',
+    rank: 9,
+  },
+];
